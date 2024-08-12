072c6f9ba72a8695d5e2d1fcc994c93cd51fa3d6
GUSTAVO GOMES MARQUES <gustavomarques178@hotmail.com>
2024-08-12 13:50:02 -0300

    ajuste endpoint

--- src/app/Services/environment.ts
+++ src/app/Services/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  local: 'https://academia-backend-production-a5af.up.railway.app/'
+  local: 'https://academia-backend-production-a5af.up.railway.app'
 };
